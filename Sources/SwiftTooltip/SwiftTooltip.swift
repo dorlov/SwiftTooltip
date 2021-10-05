a8c9667ab6df9eccba1ba2e0de2989d1192721ae
Dmitry Orlov <orlov.dmtr@gmail.com>
2021-10-05 11:07:14 +0300

    Add basic tooltip description

--- Sources/SwiftTooltip/SwiftTooltip.swift
+++ Sources/SwiftTooltip/SwiftTooltip.swift
@@ -1,3 +1,22 @@
-struct SwiftTooltip {
-    var text = "Hello, World!"
+//
+//  SwiftTooltip.swift
+//  
+//
+//  Created by Dzmitry Arlou on 10/4/21.
+//
+
+import UIKit
+
+struct Tooltip {
+    
+    var message: String
+    var calloutConfig: Bubble.CalloutConfig
+    
 }
+
+protocol TooltipHighlightedView {
+    
+    var tooltip: Tooltip { get }
+    
+}
+
